f6da1e32481161c2261e188b796ec1ff0735cd59
yaowenqiang <yaowenqiang111@163.com>
2021-05-29 14:02:33 +0800

    add cin and cout demo

--- output.cpp
+++ output.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 //using namespace std;
 using std::cout;
+using std::cin;
 
 int main()
 {
     //std::cout << "hello" << '\n';
+    cout << 34 << '\n';
     cout << "hello" << '\n';
+
+    int i = 2 + 2;
+    int j = 2;
+    cout << i << '\n';
+    i = i * 3;
+
+    cout << i << '\n';
+
+    i = 4.9;
+    j = 9 / 5;
+    cout << i << " " << j << '\n';
+
+    float f = 3.9;
+    cout << f << '\n';
+
+    f = 9 / 5;
+    f = 9.0 / 5;
+    cout << f;
+
+    cout << "Enger a number: ";
+    cin >> i;
+    cout << '\n' << i << '\n';
+
 }
